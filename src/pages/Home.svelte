6463e74fa4216fa4c2e90c9f9cf36bc77297030e
Dewi <dewitaliyapurnama@gmail.com>
2022-04-01 11:33:57 +0700

    add header

---
+++ src/pages/Home.svelte
@@ -0,0 +1,20 @@
+<script>
+	import CharityList from "../components/CharityList.svelte";
+
+	let title = "Charity";
+	let charities = ['Charity 1', 'Charity 2', 'Charity 3'];
+</script>
+
+<style>
+	h1 {
+		font-size: 3em;
+	color: #e2071c;
+    }
+</style>
+
+
+<div>
+	<h1>{title}</h1>
+	<CharityList {charities} />
+</div>
+
