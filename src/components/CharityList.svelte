6133419d9cd1305a24aeec1a29add8cafd90136f
Dewi <dewitaliyapurnama@gmail.com>
2022-04-02 09:30:38 +0700

    add header

--- src/components/CharityList.svelte
+++ src/components/CharityList.svelte
@@ -1,11 +1,14 @@
 <script>
-   export let charities = ['Local charity 1'];
+   export let charities;
 </script>
+
 <div>
     <h2>Daftar Charity</h2>
-    <pre>{charities}</pre>
-    <ul>
-        <li>Charity 1</li>
-        <li>Charity 2</li>
-    </ul>
+    {#if charities !== undefined}
+        <pre>{charities}</pre>
+        <ul>
+            <li>Charity 1</li>
+            <li>Charity 2</li>
+        </ul>
+    {/if}
 </div>
