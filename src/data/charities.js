39c22835d5510f242e228965256fea08273cf35a
Dewi <dewitaliyapurnama@gmail.com>
2022-04-06 14:55:20 +0700

    Convert HTML to svelte components

--- src/data/charities.js
+++ src/data/charities.js
@@ -5,7 +5,7 @@ module.exports = {
             title: 'First Charity Project',
             category: 'Money',
             thumbnail: 'https://live.staticflickr.com/7721/17102022497_84b7c99c342_w.jpg',
-            pledged: 0,
+            pledged: 99000,
             target: 100000,
             date_end: +new Date('10 August 2022'),
             profile_photo: 'https://live.staticflickr.com/4027/4357722810_7136f4a9e9_s.jpg',
