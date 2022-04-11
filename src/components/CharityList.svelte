f9e55e23768e08af49986f991abde66105660662
Dewi <dewitaliyapurnama@gmail.com>
2022-04-11 08:52:40 +0700

    SPA Feature

--- src/components/CharityList.svelte
+++ src/components/CharityList.svelte
@@ -166,13 +166,13 @@
 
                         <span class="xs-separetor"></span>
 
-                        <button 
-                        on:click={handleButton}
+                        <a 
+                        href="/donation/{charity.id}"
                         data-toggle="modal" 
                         data-target="#exampleModal"
                         class="btn btn-primary btn-block">
                         Donate This Cause
-                        </button>
+                    </a>
                     </div>
                     <!-- .xs-item-content END -->
                 </div>
